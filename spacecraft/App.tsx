022ad086b16be1013a9ee5587020c176e2696698
CodingCarda <carter.archie@icloud.com>
2024-11-20 18:26:58 +0000

    Working hopefully?

---
+++ spacecraft/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import LoginScreen from './src/screens/LoginScreen';
+import { TermsScreen } from "./src/screens/TermsScreen"; 
+
+function App() {
+  return (
+    <PaperProvider>
+      {/* <LoginScreen /> */}
+      <TermsScreen />
+    </PaperProvider>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default App;
