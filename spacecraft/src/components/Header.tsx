385ba31b8b4c750135ba717e240549ee7a040cc3
CodingCarda <carter.archie@icloud.com>
2024-12-05 18:09:26 +0000

    something challenge 8 something

--- spacecraft/src/components/Header.tsx
+++ spacecraft/src/components/Header.tsx
@@ -1,3 +1,21 @@
+import React from "react";
+import  { StyleSheet, Text } from "react-native";
 
 
-//<Header title="SpaceCraft"/>
+interface HeaderProps {
+   title: string;
+}
+
+
+const Header: React.
+   FC<HeaderProps> = ({ title })=> {
+       return <Text style={styles.title}> {title} </Text>
+   };
+
+
+   const styles = StyleSheet.create({
+       title:{
+
+
+       },
+   })
